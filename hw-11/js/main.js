6398201c5752359e73c3f478be00e4c4d7c40d9a
Yurii Khodzitskyi <yurochku@gmail.com>
2019-01-06 20:54:33 +0200

    added index page

---
+++ hw-11/js/main.js
@@ -0,0 +1,58 @@
+$(function(){
+    $('#menu').slicknav();
+});
+$(document).ready(function(){
+    $('.homepage-top-slider').slick({
+
+});
+});
+$(document).ready(function(){
+    $('.homepage__testimonials__slider').slick({
+        arrows : false,
+        dots : true,
+    });
+});
+
+(function() {
+
+    'use strict';
+
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
+        }
+    }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+
+})();
+var fActive='';
+function filterColor(color){
+    if(fActive!=color){
+        $('.homepage__portfolio__item').filter('.'+color).slideDown();
+        $('.homepage__portfolio__item').filter(':not(.'+color+')').slideUp();fActive=color;}
+}
+$('.f-websites').click(function(){filterColor('websites');});
+$('.f-brochoures').click(function(){filterColor('brochoures');});
+$('.f-logos').click(function(){filterColor('logos');});
+$('.f-all').click(function(){$('div').slideDown();fActive='all';});
